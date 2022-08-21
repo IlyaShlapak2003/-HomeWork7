d4c5f3bdef2de0a1fd50a10ae5bd080ce6b97f24
Илья <gosha.shlapak6982@gmail.com>
2022-08-21 23:09:14 +0300

    готовое дз 7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        lastName = lastName +" "+ firstName +" "+ middleName;
+        System.out.println("ФИО сотрудника - " + lastName);
+        // Задание 2
+        String fullName = "Ivanov Ivan Ivanovich";
+        String fullName1 = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName1);
+        // Задание 3
+        String fullName3="Иванов Семён Семёнович";
+        String[] words = fullName3.split(" ");
+        for(int i=0; i< words.length;i++){
+            if (words[i].contains("ё")){
+                words[i]=words[i].replace("ё","е");
+            }
+        }
+        fullName3 = words[0];
+        for (int i = 1; i < words.length; i++) {
+            fullName3+=" " + words[i];
+        }
+        System.out.println("Данные ФИО сотрудника - " + fullName3);
     }
 }
+// вопрос существует ли метод объединяющй слова из массива обратно в строку
